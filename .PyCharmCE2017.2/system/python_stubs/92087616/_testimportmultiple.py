07d1180c55a335e9f4e9c2a3048abbe8bc44530f
Aluno <Aluno@DESKTOP-0S24855>
2017-10-26 23:58:10 -0600

    Meu primeiro commit

---
+++ .PyCharmCE2017.2/system/python_stubs/92087616/_testimportmultiple.py
@@ -0,0 +1,15 @@
+# encoding: utf-8
+# module _testimportmultiple
+# from C:\Users\Aluno\AppData\Local\Programs\Python\Python36-32\DLLs\_testimportmultiple.pyd
+# by generator 1.145
+""" _testimportmultiple doc """
+# no imports
+
+# no functions
+# no classes
+# variables with complex values
+
+__loader__ = None # (!) real value is ''
+
+__spec__ = None # (!) real value is ''
+
